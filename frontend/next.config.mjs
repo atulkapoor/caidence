725185100300a4d65dd94900d95a6622cca6d70d
Atul Kapoor <atulkapoor@gmail.com>
2026-01-31 14:48:54 +0530

    fixes v10

---
+++ frontend/next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    async rewrites() {
+        return [
+            {
+                source: "/api/proxy/:path*",
+                destination: "http://localhost:8000/api/:path*",
+            },
+        ];
+    },
+};
+
+export default nextConfig;
